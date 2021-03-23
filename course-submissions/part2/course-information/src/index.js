8685beb6a836eb9e02e53910a4fb1e4051676654
Kevin Tian <ktian0257@gmail.com>
2021-03-23 15:59:57 -0700

    exercise 2.5: separate module - separated Course component and all child components into module

--- course-submissions/part2/course-information/src/index.js
+++ course-submissions/part2/course-information/src/index.js
@@ -1,51 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Part = ({part}) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>    
-  )
-}
-
-const Header = ({ name }) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((total, {name, exercises, id}) => total + exercises, 0)
-  // console.log(sum)
-  return (
-    <h3>total of {sum} exercises</h3>
-  ) 
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Course = ({courses}) => {
-  return (
-    <div>
-      {courses.map(course =>
-        <div key={course.id}>
-          <Header name={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
-        </div>
-      )}
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
