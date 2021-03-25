856d06a8d18be18b9f19dfbb959d2de537a6733a
Kevin Tian <ktian0257@gmail.com>
2021-03-25 14:06:33 -0700

    exercise 2.9: the phonebook step 4 - adding a case insensitive filter

--- course-submissions/part2/the-phonebook/src/App.js
+++ course-submissions/part2/the-phonebook/src/App.js
@@ -2,18 +2,26 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', phone: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
+  const [ filter, setFilter ] = useState('')
 
   const handleNewName = (event) => {
     // console.log(event.target.value)
     setNewName(event.target.value)
   }
   const handleNewPhone = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setNewPhone(event.target.value)
+  }
+  const handleFilter = (event) => {
+    // console.log(event.target.value)
+    setFilter(event.target.value)
   }
 
   const handleNewPerson = (event) => {
@@ -33,9 +41,16 @@ const App = () => {
     }
   }
 
+  // rerender by filtering persons array
+  const filtered = (filter === '') ? persons : persons.filter(person => (person.name.toLowerCase().includes(filter) || person.name.includes(filter)))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilter}/>
+      </div>
+      <h2>Add New</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -49,8 +64,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => 
-          <p key={person.name}>{person.name} {person.phone}</p>
+        {filtered.map(person => 
+          <p key={person.name}>{person.name} {person.number}</p>
         )}
       </div>
     </div>
