10d647fb41eefe60814d6c9f1df7b40ffeb7935f
Kevin Tian <ktian0257@gmail.com>
2021-03-26 16:44:30 -0700

    exercise 2.11: the phonebook step 6 - move starting state for list to db file, set up axios to get list

--- course-submissions/part2/the-phonebook/src/App.js
+++ course-submissions/part2/the-phonebook/src/App.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import PersonForm from './PersonForm'
 import Filter from './Filter'
 import Persons from './Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
   const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+    console.log('use effect running...')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled, data:')
+        console.log(response.data)
+        setPersons(response.data)
+      })
+  },[])
 
   const handleNewName = (event) => {
     // console.log(event.target.value)
